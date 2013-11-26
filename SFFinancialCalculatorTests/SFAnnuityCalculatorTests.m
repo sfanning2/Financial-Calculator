f317033252cf8894b8c38425745b5e4833c8da63
Sarah Fanning <fanningsarahe@gmail.com>
2013-11-26 15:22:15 -0600

    Final Version

---
+++ SFFinancialCalculatorTests/SFAnnuityCalculatorTests.m
@@ -0,0 +1,36 @@
+//
+//  SFAnnuityCalculatorTests.m
+//  SFFinancialCalculator
+//
+//  Created by Sarah Fanning on 11/26/13.
+//  Copyright (c) 2013 University of Nebraska - Lincoln. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SFAnnuityCalculatorTests : XCTestCase
+
+@end
+
+@implementation SFAnnuityCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testMortgage
+{
+    // n = 30
+    // y = .07
+    
+}
+
+@end
